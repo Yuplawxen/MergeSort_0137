0a0548b36780d80a4d79483349538e0edc6a3528
Yuplawxen <yuftiyannaaqurrota@gmail.com>
2026-04-02 11:05:09 +0700

    Membuat Prosedur input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,7 +5,26 @@ using namespace std;
 int arr[20], B[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan Panjang element array (maks 20): ";
+        cin >> n;
 
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nMaksimal panjang array adalah 20!" << endl;
+        }
+    }
+
+    cout << "\n----------------------------" << endl;
+    cout << "--- Inputkan Isi Elemen ---" << endl;
+    cout << "----------------------------" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Array index ke-" << i << ": ";
+        cin >> arr[i];
+    }
+}
 
 
 
